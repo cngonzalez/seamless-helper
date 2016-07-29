558c0c14e39d03312b6fefe9502f54180e7f635f
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-29 15:17:12 -0400

    made layout compatible with bootstrap

--- app/models/concerns/scrapable.rb
+++ app/models/concerns/scrapable.rb
@@ -3,17 +3,18 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_restaurant_urls(url)
-  [] = restuarant_urls
+
+  restuarant_urls = []
   search = Nokogiri::HTML(open("http://www.menupages.com/restaurants/soho-trbca-findist/all-neighborhoods/all-cuisines/"))
   search.css("table#my-search-results .link").each do |item|
     puts item.attribute("href").value
   end
 end
 
-def get_menu_items
-  items = []
-  menu_page = Nokogiri::HTML(open("http://www.menupages.com/restaurants/Acqua-at-peck-slip/menu")).css("div#restaurant-menu table")
-  menu_page.css("tr").each do |item|
+def get_menu_items(item)
+  items = [] 
+  $menu_page = Nokogiri::HTML(open("http://www.menupages.com/restaurants/Acqua-at-peck-slip/menu")).css("div#restaurant-menu table") 
+  $menu_page.css("tr").each do |item|
     my_item = {}
     my_item[:name] = item.css("th cite").text
     my_item[:ingredients] = item.css("th").text.gsub(my_item[:name], "")[1..-1]
@@ -29,6 +30,5 @@ def parse_search_url(search_term)
   ##likely this method should be paired with something that sanitizes/compares against the search terms raycent has compiled
   url = "http://http://www.menupages.com/restaurants/soho-trbca-findist/all-neighborhoods/#{search_term}"
 end
-get_menu_items(menu_page)
-[] = menu_items
-menu
+menu_items = get_menu_items($menu_page)
+
